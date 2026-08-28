4acb3b16a72b3a59c31719455298083f5dc06726
Christian Forsberg Jakobsen <1998cfj@gmail.com>
2026-08-28 23:03:28 +0200

    First commit

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, String
+from database import Base
+
+class Memory(Base):
+    __tablename__ = "memory"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    number_saved = Column(Float)
+
+class ComplexMemory(Base):
+    __tablename__ = "complex_memory"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    cartesian = Column(String)
+    real = Column(Float)
+    imaginary = Column(Float)
+    length = Column(Float)
---
+++ read_me.md
@@ -0,0 +1,5 @@
+1. Using trigonometric functions:
+- If you want to find sin/cos of for example pi/2 make sure mode is "radians", set multiply_with_pi to "yes" and a = 0.5. Pi will then be added automatically by the function.
+
+2. Complex functions:
+- The complex functions return str due to FastAPI not accepting Python´s complex type.
